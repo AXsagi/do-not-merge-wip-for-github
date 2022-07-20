225d9f8d38e2f46493f9b8eb555b059af8128c60
Sagi Haas <sagi.haas@axonius.com>
2022-07-20 18:27:32 +0300

    My other options

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,9 +1,20 @@
 (() => {
   'use strict';
 
-  function changeMergeButtonState() {
+  async function changeMergeButtonState() {
     let container = document.querySelector('#js-repo-pjax-container');
     let issueTitle = container.querySelector('.js-issue-title').textContent;
+    var waitASecond = function() {
+      return new Promise(resolve => {
+          setTimeout(function() {
+          resolve("OK");
+          }, 1000);
+      });
+    };
+    while (document.querySelector('.status-meta') === null){
+      await waitASecond();
+    }
+    let statusMeta = document.querySelector('.status-meta').textContent;
     let buttonMerges = container.querySelectorAll('.merge-message button[data-details-container]');
     let buttonMergeOptions = container.querySelectorAll('.merge-message button[data-details-container] + .select-menu-button');
     let disabled = false;
@@ -15,9 +26,11 @@
       let localStorage = response.localStorage;
       const wipTitleRegex = /[\[(^](do\s*n[o']?t\s*merge|wip|dnm)[\]):]/i;
       const wipTagRegex = /(wip|do\s*not\s*merge|dnm)/i;
+      const oneApproverText = 'At least 1 approving review is required';
 
       const isWipTitle = wipTitleRegex.test(issueTitle);
       const isWipTaskList = container.querySelector('.timeline-comment') && container.querySelector('.timeline-comment').querySelector('input[type="checkbox"]:not(:checked)') !== null;
+      const noOneApproved = statusMeta.indexOf(oneApproverText) !== -1;
       let isSquashCommits = false;
       for (const commitMessage of container.querySelectorAll('.commit-message')) {
         isSquashCommits = isSquashCommits || commitMessage.textContent.match(/(squash|fixup)!/);
@@ -28,24 +41,26 @@
         isWipTag = isWipTag || label.textContent.match(wipTagRegex);
       }
 
-      disabled = (isWipTitle || isWipTaskList || isSquashCommits || isWipTag);
+      disabled = (isWipTitle || isWipTaskList || isSquashCommits || isWipTag || noOneApproved);
 
       let buttonMessage = '';
 
       if (localStorage && localStorage.buttonMessage) {
         buttonMessage = localStorage.buttonMessage;
       } else {
-        buttonMessage = 'WIP! You can\'t merge!';
+        buttonMessage = 'Approve First!!!';
       }
 
       buttonHtml = disabled ? buttonMessage : 'Merge pull request';
-
-      for (const buttonMerge of buttonMerges) {
-        buttonMerge.disabled = disabled;
-        buttonMerge.innerHTML = buttonHtml;
-      }
-      for (const buttonMergeOption of buttonMergeOptions) {
-        buttonMergeOption.disabled = disabled;
+      if (disabled) {
+        document.querySelector('.js-admin-merge-override').disabled = true;
+        for (const buttonMerge of buttonMerges) {
+          buttonMerge.disabled = disabled;
+          buttonMerge.innerHTML = buttonHtml;
+        }
+        for (const buttonMergeOption of buttonMergeOptions) {
+          buttonMergeOption.disabled = disabled;
+        }
       }
 
       // unset variables
