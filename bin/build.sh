3e891da0aa09b2f36202d69031aed5716859f56c
Sagi Haas <sagi.haas@axonius.com>
2022-07-21 12:20:18 +0300

    cleanup

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euv
 
-if [[ "${TRAVIS_TAG}" ]]; then
-  npm run build
-  ls -a package/
-  echo "ziped!"
-else
-  echo "Not Release"
-fi
+npm run build
+ls -a package/
+echo "ziped!"
